ea13e583a9dd5eb87c3c06e40e93e722d2a4ce41
Ifeanyichukwu Onyekwelu <ifeanyionyekwelu786@gmail.com>
2025-12-14 07:08:16 +0100

    updated docker cmd value

--- server/run.py
+++ server/run.py
@@ -12,4 +12,4 @@ def make_shell_context():
 
 if __name__ == "__main__":
     port = int(PORT)
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port, debug=True, allow_unsafe_werkzeug=True)
